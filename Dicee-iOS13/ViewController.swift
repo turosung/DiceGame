5611b673c6099a01801cf7a993b5033f5a833602
Nuhu Sulemana <turosung@gmail.com>
2023-03-22 07:36:39 +0000

    Created UI for dice game and linked it to ViewController

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceOne")
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceThree")
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceFour")
+    }
+    
 }
 
